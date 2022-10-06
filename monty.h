8abe50520ffc2910850615495abc648108b1be6d
CollinsNasongo <nasongocollins@gmail.com>
2022-10-07 01:46:37 +0300

    Tested files

--- monty.h
+++ monty.h
@@ -1,7 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
+#define  _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-
+#define DELIMITER "\n\t\r "
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -9,8 +14,8 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
+ * for stack, queues, LIFO, FIFO Holberton project
+ **/
 typedef struct stack_s
 {
 	int n;
@@ -24,13 +29,31 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
+ * for stack, queues, LIFO, FIFO Holberton project
+ **/
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
-}
-instruction_t;
+} instruction_t;
 
+/**
+* struct global_s - hold global variables
+* @return_val: return value
+* @mol: mode of list
+* @cc: code command
+* @apc: push command
+**/
+
+extern instruction_t opcodes[];
+unsigned int len(stack_t **stack);
+void free_stack(stack_t *stack);
+void op_nop(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_push(stack_t **stack, char *n, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+int find_opcode(stack_t **stack, char *opcode, int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+size_t lol(stack_t *h);
 #endif
